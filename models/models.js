e73ab2f3643f25f9e55029d7fd8cd1abdf865102
rvg <rvg55@hotmail.com>
2015-07-09 20:42:55 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -31,15 +31,19 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en la DB
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({
                     pregunta: 'Capital de Italia',
       	            respuesta: 'Roma'
-      	         })
-      .success(function(){console.log('Base de datos inicializada')});
+      	         });
+      Quiz.create({
+                     pregunta: 'Capital de Portugal',
+       	            respuesta: 'Lisboa'
+       	         })
+      .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// router.get('/quizes/question', quizController.question);
+// router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/amswer', quizController.answer);
 
 router.get('/author', function(req, res, next) {
   res.render('author', { author: 'Raul' });
