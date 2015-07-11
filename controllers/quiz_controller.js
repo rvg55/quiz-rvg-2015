8f6c3433c7379c1caba7f213655f07fa61d5df36
rvg <rvg55@hotmail.com>
2015-07-11 12:42:55 +0200

    Searching with filter text

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,7 +4,7 @@ var models = require('../models/models.js');
 // autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
-    function(quizes){
+    function(quiz){
       if(quiz){
         req.quiz = quiz;
         next();
@@ -16,10 +16,25 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes){
-      res.render('quizes/index.ejs', {quizes: quizes});
-    }).catch(function(error) {next(error);});
+  if(req.query.search !== undefined) {
+    var search = req.query.search.replace(/\s/g,'%');
+    search = '%'+search+'%';
+    console.log("Buscando: "+search);
+    models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
+      function(quizes){
+        if(quizes){
+          res.render('quizes/index', {quizes: quizes});
+        } else {
+          next(new Error('No existen preguntas con el texto: '+req.query.texto));
+        };
+      }
+    ).catch(function(error) {next(error);});
+  } else {
+    models.Quiz.findAll().then(
+      function(quizes){
+        res.render('quizes/index', {quizes: quizes});
+      }).catch(function(error) {next(error);});
+  }
 };
 
 // GET /quizes/:id
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,10 @@ router.get('/', function(req, res, next) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // Autoload :quizId
 
-// router.get('/quizes/question', quizController.question);
-// router.get('/quizes/answer', quizController.answer);
 router.get('/quizes/', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+// router.get('/quizes/search', quizController.search);
 
 router.get('/author', function(req, res, next) {
   res.render('author', { author: 'Raul' });
