76a79a31b8602d9be3d5b34a68f68b1c853dc4ab
rvg <rvg55@hotmail.com>
2015-07-12 21:41:29 +0200

    added validation

--- models/quiz.js
+++ models/quiz.js
@@ -1,13 +1,18 @@
 // Definicion del modelo de Quiz
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Quiz',
-    { pregunta: {
+    {
+      pregunta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta pregunta" }}
+        validate: {
+          notEmpty: {msg: "-> Falta pregunta" }
+        }
       },
       respuesta: {
         type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta respuesta" }}
+        validate: {
+          notEmpty: {msg: "-> Falta respuesta" }
+        }
       }
     }
   );
