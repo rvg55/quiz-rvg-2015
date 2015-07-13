dded5ddbec02049fb71465d76bfabb868b5f9ecf
rvg <rvg55@hotmail.com>
2015-07-13 19:25:09 +0200

    updating

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
-// var users = require('./routes/users');
 
 var app = express();
 
@@ -24,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
